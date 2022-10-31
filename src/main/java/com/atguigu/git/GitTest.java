e4b5e12531393c0856d4d25d7cfbf0ec404c2133
Lqs <Lqs@atguigu.com>
2022-10-31 08:46:54 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
+        System.out.println("push test");
 
     }
 

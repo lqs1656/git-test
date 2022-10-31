d47c9649dbd154b35be2cfbfa35ec91b28fe08f8
lqs1656 <58756991+lqs1656@users.noreply.github.com>
2022-10-31 08:52:59 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 
